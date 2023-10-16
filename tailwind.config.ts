f7d10cb85e1b39be80177af21daa9b1c39b31e0d
thanhnd2512@gmail.com <thanhnd2512@gmail.com>
2023-10-16 10:11:38 +0700

    tailwind configuration, setup project structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        p: "240px",
+        d: "1024px",
       },
+      fontFamily: {
+        //system font
+        OpenSans: ["Open Sans", "sans-serif"],
+        LexendDeca: ["Lexend Deca", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
+        //custom font
+        NunitoSans: ["Nunito Sans", "sans-serif"],
+        SVNDancing: ["SVN-Dancing", "sans-serif"],
+      },
+
+      colors: {
+        //system colors
+        redPrimmary: "#ed1b2e",
+        Blue3D: "#1D1D3D",
+        colortopdownBlue: "#2c95ff",
+        colortopdownGray: "#414045",
+        blueAdmin: "#74affc",
+        purpleAdmin: "#bb86fc37",
+        purpleHover: "#BB86FC",
+        BlueFF: "#1A49FF",
+        //Custom colors
+        mainblue: "#2e9279",
+        mainwhite: "#f2f2f2",
+        mainorange: "#f3b41e",
+      },
+
+      // custom
+      backgroundImage: {},
+      backgroundColor: {},
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
